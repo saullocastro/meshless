04419130b5fbfe9888c2bdac77e36600085be6c8
saullocastro <saullogiovani@gmail.com>
2016-12-23 12:58:11 -0200

    Test

--- python/beam2d.py
+++ python/beam2d.py
@@ -5,17 +5,17 @@ from scipy.spatial import cKDTree
 import matplotlib.pyplot as plt
 
 class P(object):
-    """
+    r"""
           y, v
 
-          4
-          |
-      1-- o -- 2  x, u
-          |
-          3
+        8 4 6
+         \|/
+      1-- o -- 2   x, u
+         /|\
+        5 3 7
 
     """
-    def __init__(self):
+    def __init__(self, size):
         self.rho = 5.e3
         self.i = 0
         self.j = 0
@@ -28,87 +28,95 @@ class P(object):
 
         self.E = 71e9
         self.nu = 0.33
-        self.length = 0.01
-        self.h = 0.01
-        self.b = 0.01
+        self.size = size
+        self.h = 0.001
         self.x = 0
         self.y = 0
+        self.pos_1 = np.array([0., 0.])
         self.pos = np.array([0., 0.])
 
-        self.u_1 = np.array([0, 0.]) # 2-D u, v
-        self.u = np.array([0, 0.]) # 2-D u, v
-
-        self.ut_1 = np.array([0, 0.]) # 2-D uxx, vxx, rzxx
         self.ut = np.array([0, 0.]) # 2-D uxx, vxx, rzxx
 
-        self.utt_1 = np.array([0, 0.]) # 2-D uxx, vxx, rzxx
         self.utt = np.array([0, 0.]) # 2-D uxx, vxx, rzxx
 
         self.f = np.array([0., 0.]) # 2-D
-        self.fext = np.array([0, 0]) # 2-D
+        self.fext = np.array([0., 0.]) # 2-D
 
         self.build()
 
 
     def build(self):
-        self.A = self.h * self.b
-        #kuu = self.E*self.A
-        #kvv = self.E*self.A
-        #self.k = np.array([[kuu, kuv],
-                           #[kvu, kvv]])
-        #self.kinv = np.linalg.inv(self.k)
-        # mass matrix
-        self.m = self.length * self.A * self.rho
+        self.A = self.h * self.size
+        self.m = self.size * self.A * self.rho
 
     def __str__(self):
-        return 'P%d%d' % (self.i, self.j)
+        return 'P%02d%02d' % (self.i, self.j)
     def __repr__(self):
         return self.__str__()
 
-# building up particle connectivity
-near = 4
-lenx = 200
-leny = 100
-size = 10
-numx = lenx//size
-numy = leny//size
-ps = [P() for _ in range(numx * numy)]
+# building particles
+lenx = 1
+leny = 0.5
+size = 0.1
+numx = int(lenx/size)
+numy = int(leny/size)
+ps = [P(size) for _ in range(numx * numy)]
 
+# positions
+psdict = {}
 for i in range(numx):
+    psdict[i] = {}
     for j in range(numy):
-        #TODO update position x and y
-        # recalculate force components among particles according to the
-        # updated x and y position
         p = ps[j*numx + i]
-        p.pos[0] = lenx*i/(numx-1)
-        p.pos[1] = leny*j/(numy-1)
+        psdict[i][j] = p
+        p.x = lenx*i/(numx-1)
+        p.y = leny*j/(numy-1)
+        p.pos[0] = p.x
+        p.pos[1] = p.y
+        p.pos_1[:] = p.pos
         p.i = i
         p.j = j
 
-posall = [p.pos for p in ps]
-tree = cKDTree(posall)
+# connectivity
+for i in range(numx):
+    for j in range(numy):
+        p = psdict[i][j]
+        if i != 0:
+            p.padjs.append(psdict[i-1][j])
+            if j > 0:
+                p.padjs.append(psdict[i-1][j-1])
+            if j < numy-1:
+                p.padjs.append(psdict[i-1][j+1])
+        if i != numx-1:
+            p.padjs.append(psdict[i+1][j])
+            if j > 0:
+                p.padjs.append(psdict[i+1][j-1])
+            if j < numy-1:
+                p.padjs.append(psdict[i+1][j+1])
+        if j != 0:
+            p.padjs.append(psdict[i][j-1])
+        if j != numy-1:
+            p.padjs.append(psdict[i][j+1])
 
 for p in ps:
-    distvec, posvec = tree.query(p.pos, k=(near+1))
-    distvec = distvec[1:]
-    posvec = posvec[1:]
-    p.dadjs = distvec
-    p.padjs = [ps[i] for i in posvec]
+    for padj in p.padjs:
+        dist = ((p.pos - padj.pos)**2).sum()**0.5
+        p.dadjs.append(dist)
 
 fig = plt.figure(dpi=500, figsize=(10, 10))
 fig.clear()
 ax = plt.gca()
-#ax.set_xlim(-size, lenx+size)
-#ax.set_ylim(-size, leny+size)
+#ax.set_xlim(-2*size, lenx+2*size)
+#ax.set_ylim(-2*size, leny+2*size)
 ax.plot([p.pos[0] for p in ps], [p.pos[1] for p in ps], 'ko')
 ax.set_aspect('equal')
 fig.savefig(filename='tmp_beam2d_points.png', bbox_inches='tight')
 
 # integration
-dt = 0.000001
-n = 500
+dt = 0.0001
+n = 200
 
-ps[-1].fext += [0, -1]
+psdict[numx-1][numy-1].fext += [0, 100]
 
 for step in range(n):
     for p in ps:
@@ -117,41 +125,49 @@ for step in range(n):
         p.f += p.fext
         dist_init = p.dadjs
         for i, padj in enumerate(p.padjs):
-            pos_diff = p.pos - padj.pos
+            #TODO not considering area reduction deformedwith dist variation
+            k = p.E * p.A / dist_init[i]
+            pos_diff = p.pos_1 - padj.pos_1
             if pos_diff.sum() == 0:
                 continue
             dx = pos_diff[0]
             dy = pos_diff[1]
             d = (dx**2 + dy**2)**0.5
-            #TODO not considering area reduction with dist variation
-            k = p.E * p.A / dist_init[i]
-            #FIXME treat component-wise
             fres = (d - dist_init[i]) * k
-            fx = fres * dy / d
-            fy = fres * dx / d
+            fx = -fres * dx / d
+            fy = -fres * dy / d
             p.f += [fx, fy]
 
-        p.u = p.u_1 + p.ut_1*dt + p.utt_1*dt**2
-        p.ut = p.ut_1 + p.utt_1*dt
         p.utt = 1./p.m*p.f
-        if np.any(np.isnan(p.u)):
-            print p.u
-            raise
+        p.ut = p.ut + p.utt*dt
+        p.pos = p.pos_1 + p.ut*dt
 
-        p.u_1 = p.u.copy()
-        p.ut_1 = p.ut.copy()
-        p.utt_1 = p.utt.copy()
+    for p in ps:
+        if np.any(np.isnan(p.pos)):
+            print(p.pos)
+            raise RuntimeError()
+        p.pos_1[:] = p.pos
 
         # clamping one side
         if p.i in [0]:
-            p.u_1 *= 0
-            p.ut_1 *= 0
-            p.utt_1 *= 0
-            p.u *= 0
+            p.pos[:] = [p.x, p.y]
+            p.pos_1[:] = [p.x, p.y]
             p.ut *= 0
             p.utt *= 0
 
-        p.pos += p.u
-
-ax.plot([p.pos[0] for p in ps], [p.pos[1] for p in ps], 'r^', mfc='None')
+xplot = [p.pos[0] for p in ps]
+yplot = [p.pos[1] for p in ps]
+ax.plot(xplot, yplot, 'r^', mfc='None')
 fig.savefig(filename='tmp_beam2d_deformed.png', bbox_inches='tight')
+
+if True:
+    fig.clear()
+    ax = fig.gca()
+    ax.set_aspect('equal')
+    xplot = np.array([p.x for p in ps]).reshape(numy, numx)
+    yplot = np.array([p.y for p in ps]).reshape(numy, numx)
+    fxplot = np.array([p.f[1] for p in ps]).reshape(numy, numx)
+    levels = np.linspace(fxplot.min(), fxplot.max(), 400)
+    ax.contourf(xplot, yplot, fxplot, levels=levels)
+    fig.savefig(filename='tmp_beam2d_fx.png', bbox_inches='tight')
+

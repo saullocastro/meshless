BUG: fixed K0s calculation

meshless/espim/plate2d_calc_kG.py
@@ -9,8 +9,8 @@ from .read_mesh import getMid
 
 def calc_kG(d, mesh, prop_from_node, silent=True):
     msg('Calculating KG...', silent=silent)
-    n = d.shape[0] // 5
     dof = 5
+    n = d.shape[0] // dof
 
     #TODO allocate less memory here...
     kG = np.zeros((n*dof, n*dof), dtype=np.float64)

meshless/linear_buckling.py
@@ -64,8 +64,7 @@ def lb(K, KG, tol=0, sparse_solver=True, silent=False,
             eigvals, peigvecs = eigsh(A=KG, k=k,
                     which='SM', M=K, tol=tol, sigma=1., mode=mode)
             msg('finished!', level=3, silent=silent)
-            eigvecs = np.zeros((sizebkp, num_eigvalues),
-                               dtype=peigvecs.dtype)
+            eigvecs = np.zeros((sizebkp, num_eigvalues), dtype=peigvecs.dtype)
             eigvecs[used_cols, :] = peigvecs
 
     else:
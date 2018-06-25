b680987fbd6970f7f2c5ba1609e994de98d630d2
saullocastro <saullogiovani@gmail.com>
2018-06-25 18:34:37 +0200

    STY

--- meshless/espim/plate2d_add_k0s.py
+++ meshless/espim/plate2d_add_k0s.py
@@ -1,6 +1,5 @@
 from meshless.espim.plate2d_add_k0s_cell_based import add_k0s as add_k0s_cell
-from meshless.espim.plate2d_add_k0s_cell_based_no_smoothing import (add_k0s as
-        add_k0s_cell_no_smoothing)
+from meshless.espim.plate2d_add_k0s_cell_based_no_smoothing import add_k0s as add_k0s_cell_no_smoothing
 from meshless.espim.plate2d_add_k0s_edge_based import add_k0s as add_k0s_edge
 
 def add_k0s(k0, mesh, prop_from_node, method='cell-based'):

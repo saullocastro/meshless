6319cd6027cfc2bf4a4929f473e40e68a76911d9
Saullo Giovani Pereira Castro <saullogiovanip@tudelft.net>
2019-01-23 13:57:45 +0100

    BUG: fixed lagrange interpolation, MAINT: removed ND version

--- meshless/interpolate.py
+++ meshless/interpolate.py
@@ -1,72 +1,51 @@
 import numpy as np
 
-def interpolate_lagrange_ND(x, x_nodes, field_nodes):
-    """Interpolate values using ND Lagrangian polynomials
-
-    The order of the polynomial interpolation in each direction depends on the
-    number of field nodes passed to this function
-
-    Parameters
-    ----------
-    x : (N, D) array_like
-        Coordinates used to interpolate
-    x_nodes : (K, D) array_like
-        Nodal coordinates
-    field_nodes : (K) array_like
-        Field to be interpolated at corresponding nodes
-
-    Returns
-    -------
-    field : (N,) array_like
-        Interpolated field
-
-    """
-    x = np.asarray(x)
-    x_nodes = np.asarray(x_nodes)
-    x = np.atleast_2d(x).reshape(x.shape[0], -1)
-    x_nodes = np.atleast_2d(x_nodes).reshape(x_nodes.shape[0], -1)
-    #TODO remove loops one day
-    field = 0
-    for m, (xs, theta) in enumerate(zip(x_nodes, field_nodes)):
-        tmp = 1
-        for dim, xi in enumerate(xs):
-            for k, xk in enumerate(x_nodes[:, dim]):
-                if k != m and (xi - xk) != 0:
-                    tmp *= (x[:, dim]- xk)/(xi - xk)
-        field += tmp * theta
-    return field
-
 
 def interpolate_lagrange_2D(x, y, x_nodes, y_nodes, field_nodes):
     """Interpolate values using 2D Lagrangian polynomials
 
     The order of the polynomial interpolation in each direction depends on the
-    number of field nodes passed to this function
+    number control nodes passed for dimensions x and y.
 
     Parameters
     ----------
-    x, y : (N,) array_like
+    x, y : (K,) array_like
         Coordinates used to interpolate
-    x_nodes, y_nodes, field_nodes : array_like
-        Nodal coordinates and field to be interpolated at corresponding nodes
+    x_nodes : (M,) array_like
+        Coordinates of dimension 1
+    y_nodes : (N,) array_like
+        Coordinates of dimension 2
+    field_nodes (M, N): array_like
+        Nodal coordinates and field to be interpolated at corresponding control
+        nodes
 
     Returns
     -------
-    field : (N,) array_like
+    field : (K,) array_like
         Interpolated field
 
     """
+    x_nodes = np.asarray(x_nodes)
+    y_nodes = np.asarray(y_nodes)
+    field_nodes = np.asarray(field_nodes)
+    assert field_nodes.ndim == 2
+    assert field_nodes.shape[0] == x_nodes.shape[0]
+    assert field_nodes.shape[1] == y_nodes.shape[0]
     #TODO try to remove loops one day
     field = 0
-    for m, (xm, ym, theta) in enumerate(zip(x_nodes, y_nodes, field_nodes)):
-        tmp = 1
-        for k, xk in enumerate(x_nodes):
-            if k != m and (xm - xk) != 0:
-               tmp *= (x - xk)/(xm - xk)
-        for p, yp in enumerate(y_nodes):
-            if p != m and (ym - yp) != 0:
-               tmp *= (y - yp)/(ym - yp)
-        field += tmp * theta
+    for m in range(len(x_nodes)):
+        xm = x_nodes[m]
+        fx = 1
+        for i, xi in enumerate(x_nodes):
+            if i != m and (xm - xi) != 0:
+               fx *= (x - xi)/(xm - xi)
+        for n in range(len(y_nodes)):
+            yn = y_nodes[n]
+            fy = 1
+            for j, yj in enumerate(y_nodes):
+                if j != n and (yn - yj) != 0:
+                   fy *= (y - yj)/(yn - yj)
+            field += fx * fy * field_nodes[m, n]
     return field
 
 
@@ -78,10 +57,17 @@ def interpolate_lagrange_3D(x, y, z, x_nodes, y_nodes, z_nodes, field_nodes):
 
     Parameters
     ----------
-    x, y, z : (N,) array_like
+    x, y : (K,) array_like
         Coordinates used to interpolate
-    x_nodes, y_nodes, z_nodes, field_nodes : array_like
-        Nodal coordinates and field to be interpolated at corresponding nodes
+    x_nodes : (M,) array_like
+        Coordinates of dimension 1
+    y_nodes : (N,) array_like
+        Coordinates of dimension 2
+    z_nodes : (P,) array_like
+        Coordinates of dimension 3
+    field_nodes (M, N, P): array_like
+        Nodal coordinates and field to be interpolated at corresponding control
+        nodes
 
     Returns
     -------
@@ -89,18 +75,33 @@ def interpolate_lagrange_3D(x, y, z, x_nodes, y_nodes, z_nodes, field_nodes):
         Interpolated field
 
     """
+    x_nodes = np.asarray(x_nodes)
+    y_nodes = np.asarray(y_nodes)
+    z_nodes = np.asarray(z_nodes)
+    field_nodes = np.asarray(field_nodes)
+    assert field_nodes.ndim == 3
+    assert field_nodes.shape[0] == x_nodes.shape[0]
+    assert field_nodes.shape[1] == y_nodes.shape[0]
+    assert field_nodes.shape[2] == z_nodes.shape[0]
     #TODO try to remove loops one day
     field = 0
-    for m, (xm, ym, zm, theta) in enumerate(zip(x_nodes, y_nodes, z_nodes, field_nodes)):
-        tmp = 1
-        for k, xk in enumerate(x_nodes):
-            if k != m and (xm - xk) != 0:
-               tmp *= (x - xk)/(xm - xk)
-        for p, yp in enumerate(y_nodes):
-            if p != m and (ym - yp) != 0:
-               tmp *= (y - yp)/(ym - yp)
-        for p, zp in enumerate(z_nodes):
-            if p != m and (zm - zp) != 0:
-               tmp *= (z - zp)/(zm - zp)
-        field += tmp * theta
+    for m in range(len(x_nodes)):
+        xm = x_nodes[m]
+        fx = 1
+        for i, xi in enumerate(x_nodes):
+            if i != m and (xm - xi) != 0:
+               fx *= (x - xi)/(xm - xi)
+        for n in range(len(y_nodes)):
+            yn = y_nodes[n]
+            fy = 1
+            for j, yj in enumerate(y_nodes):
+                if j != n and (yn - yj) != 0:
+                   fy *= (y - yj)/(yn - yj)
+            for p in range(len(z_nodes)):
+                zp = z_nodes[p]
+                fz = 1
+                for k, zk in enumerate(y_nodes):
+                    if k != p and (zp - zk) != 0:
+                       fz *= (z - zk)/(zp - zk)
+                field += fx * fy * fz * field_nodes[m, n, p]
     return field
